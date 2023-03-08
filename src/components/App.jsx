9bbfece3f3dd971e03edd8181a60982f5c1a1d43
Mr. Coder <aashryashrivastava@gmail.com>
2023-03-08 22:33:14 +0530

    useState Hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,21 @@
-import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
-
-function createCard(contact) {
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
-    />
-  );
-}
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+  const [count, setCount] = useState(0);
 
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
     </div>
   );
 }
